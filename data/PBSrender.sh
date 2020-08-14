c097f22bf61eb3e81462d3ff97cdfb2a91ce83bd
jperales <jperales.bioinfo@gmail.com>
2020-08-14 16:19:40 +0200

    Fix outdir issue in pandoc by symbolic links

--- data/PBSrender.sh
+++ data/PBSrender.sh
@@ -10,13 +10,4 @@ source ~/.bashrc
 conda activate ../envs/scrna
 export RSTUDIO_PANDOC="$CONDA_PREFIX/bin/pandoc"
 
-# Original in local
-# ${CONDA_PREFIX}/bin/R -e "rmarkdown::render('$<', output_file='$@.md', params=list(ID='$(@:%_Seurat=%)',OUTDIR='$@'))"
-# mv $@ $(filter-out $<,$^)
-# mv $@.md $(filter-out $<,$^)
-# mv $@.html $(filter-out $<,$^)
-
 ${CONDA_PREFIX}/bin/R -e "rmarkdown::render('${RMD}', output_file='${PREFIX}.md', params=list(ID='$SAMPLEID',OUTDIR='${PREFIX}'))"
-mv ${PREFIX} ${DEST}
-mv ${PREFIX}.md ${DEST}
-mv ${PREFIX}.html ${DEST}
